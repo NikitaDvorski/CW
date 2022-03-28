5dd02b9784672c39eb421747d910ba39782d04d3
NikitaDvorski <nikdvorski@gmail.com>
2022-03-28 21:17:26 +0300

    initial commit

--- src/main/java/cw/Main.java
+++ src/main/java/cw/Main.java
@@ -7,9 +7,9 @@ public class Main {
                 {4,1,1},
                 {-2,2,-1}
         };
-        for (int i=0;i< args.length;i++){
+        for (int i=0;i< matrixA.length;i++){
             for (int j=0;j< matrixA.length;j++){
-                System.out.println(matrixA[i][j] + ' ');
+                System.out.print(matrixA[i][j] + " ");
             }
             System.out.println();
         }
